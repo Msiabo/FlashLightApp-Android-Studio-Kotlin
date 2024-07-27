8ca00b272d1e92074f0cc45fce3a1aea611bd349
Siyabonga Mhlongo <151571750+Msiabo@users.noreply.github.com>
2024-07-28 00:49:57 +0200

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.flashlight
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        bind
+    }
+}
